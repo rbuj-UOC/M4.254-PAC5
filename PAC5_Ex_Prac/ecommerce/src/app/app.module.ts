aeadc1e3fdbf5d730f442ff7b2cf8f4421945710
rbuj <robert.buj@gmail.com>
2024-05-14 18:26:29 +0200

    Exercici 5

--- PAC5_Ex_Prac/ecommerce/src/app/app.module.ts
+++ PAC5_Ex_Prac/ecommerce/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +22,8 @@ import { ArticleNewReactiveComponent } from './articles/article-new-reactive/art
   imports: [
     AppRoutingModule,
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
